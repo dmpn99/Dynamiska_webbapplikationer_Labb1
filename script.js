5a0849b96f21c87f046952a71005fb181bde33d5
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2025-11-21 08:15:48 +0100

    .

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// Övning 2
+//---------------  Övning 2 ----------------
 
 //console.log("Före blocket:" + testLet); // Fungerar inte
 //console.log("Före blocket:" + testConst); // Fungerar inte
 //console.log("Före blocket:" + testVar); // Fungerar men variabeln blir undefined
 
 
+
 {
    let testLet = "Testar variabeln let";
     const testConst = "Testar variabeln const";
-    var testVar = "Testar variabeln var";
-
+    var testVar = "Testar variabeln var";   
     console.log(testLet); // Fungerar
     console.log(testConst); // Fungerar
     console.log(testVar); // Fungerar
-    
 }
 
 
@@ -23,13 +22,18 @@ console.log("Efter blocket:" + testVar); // Fungerar
 
 /* Reflektion:
 Före blocket: Den enda variabeln som programmet inte kraschar av är var -den blir undefined.
+var har Function Scope, det gör att den struntar i blocket{...}. let och const har block scope och
+finns inte tillgängliga utanför blocket.
 
 Inne i blocket: Alla tre variabler fungerar som de ska.
+let och const har block scope, så de är tillgängliga inuti blocket.
+var har function scope.
 
-Efter blocket: Återigen kraschar programmet för let och const, men var -fungerar det som det ska.
+Efter blocket: Återigen kraschar programmet för let och const, men var -fungerar som det ska.
+var har function scope, vilket gör att den är tillgänglig utanför blocket.
 */
 
-// Övning 3
+//---------------  Övning 3 ----------------
 
 console.log(3==3); // Lös jämförelse - True
 console.log(3===3); // Strikt jämförelse - True
@@ -50,24 +54,42 @@ console.log(test);
 1. Skillnaden mellan == och === är att === kräver samma datatyp,
     medans == försöker omvandla typerna. 
 
-2. En ternary operator omvandlas värdet till en bool.
+2. Med en ternary operator omvandlas värdet till en bool.
    Eftersom undefined är ett "falsy" värde, väljer koden det andra alternativet.
 
-   NaN står för Not a Number och är inte lika med något.
-   Undefined är en variabel som inte är deklarerad.
-   Null är ett deklarerat tomt värde.
+   NaN står för Not a Number och är inte lika med något, inte ens sig själv.
+   Undefined är en variabel som inte har blivit tilldelad något värde.
+   Null är ett deklarerat tomt värde, kan användas för att nollställa en variabel.
 */
 
-// Övning 4
-let name = "Nellie";
+//---------------  Övning 4 ----------------
 
-console.log("Före " + name); // Resultat Nellie
+let name = "Nellie"; // Global variabel
 
-function greet(name)
-{
-    console.log("Inuti " + name); // Resultat Fredrik
-    return "Som return " + name; // Reultat Fredrik
+console.log("Före: " + name); // Utskrift: Nellie
+
+// Jag valde en Funktionsdeklaration
+function greet(name) {
+    // Här inne är 'name' parametern, inte den globala variabeln
+    return "Hej " + name;
 }
-console.log(greet("Fredrik")); // Resultat Fredrik
 
+// Anrop med argumentet "Fredrik"
+console.log(greet("Fredrik")); 
 
+console.log("Efter: " + name); // Utskrift: Nellie (Globala variabeln är oförändrad)
+
+/* Reflektion Uppgift 4:
+
+1. Jag valde en vanlig funktionsdeklaration för att det kändes enklast. 
+   Skillnaden mot arrow-funktioner och uttryck är hur man skriver dem och hur de laddas.
+
+2. Eftersom jag valde en deklaration kan jag anropa funktionen var som helst, även innan den skapats i koden. 
+   Dom andra sätten kräver att funktionen skapas före man anropar.
+
+3. Inuti funktionen gäller parametern "name", den krockar inte med variabeln "name" som ligger utanför. 
+
+4. Variabel = Behållare för data.
+   Parameter = Namnet vi sätter i funktionens parentes.
+   Argument = De värdet ("Fredrik") vi skickar in när vi kör funktionen.
+*/
